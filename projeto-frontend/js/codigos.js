b442762b0e9664e96974c7955e3bb2ccdd719b13
Luís Filipe Pereira Perrú <lfpperru@hotmail.com>
2021-05-23 11:39:24 -0300

    Segunda versão do projeto final de Frontend Essencial

--- projeto-frontend/js/codigos.js
+++ projeto-frontend/js/codigos.js
@@ -1,7 +1,27 @@
-function mostrar(id) {
-  if (document.getElementById(id).style.display == "block") {
+function exibirSinopse(id) {
+  if (document.getElementById(id).style.display === "block"){
     document.getElementById(id).style.display = "none";
-  } else {
-    document.getElementById(id).style.display = "block";
+    document.getElementById("button-"+id).value = "Sinopse"
   }
+    
+  else{
+    document.getElementById(id).style.display = "block";
+    document.getElementById("button-"+id).value = "Ocultar"
+  }
+}
+
+// function validarCampos() {
+//   if (document.getElementById('name').value === "")
+//     document.getElementById('name').classList.add("classeComErro");
+//   if (document.getElementById('email').value === "")
+//     document.getElementById('email').classList.add("classeComErro");
+//   if (document.getElementById('cpf').value === "")
+//     document.getElementById('cpf').classList.add("classeComErro");
+// }
+
+function exibirMensagem() {
+    // if (document.getElementById('nome').classList.contains("input:invalid") || document.getElementById('email').classList.contains("input:invalid") || document.getElementById('cpf').classList.contains("input:invalid"))
+      document.querySelectorAll('input').forEach(x => x.value = '');
+    // else
+      alert("Sua nota fiscal foi enviada com sucesso!");
 }
